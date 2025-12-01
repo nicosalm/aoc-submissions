9e4c1d2fa6acd4d9909982bebc92ee11af36bbdc
fallow64 <fallow640@gmail.com>
2025-12-01 02:45:05 -0600

    Edit metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "UPL AdventOfCode Submissions",
-  description: "A collection of AdventOfCode submissions by UPL members.",
+  title: "AOC Submission Collection",
+  description:
+    "A collection of AdventOfCode code submissions for the Undergraduate Project Lab. Compare your solutions with others!",
 };
 
 export default function RootLayout({
